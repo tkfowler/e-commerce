69287e3465660344a5b8e73eb567c4a92a2a043e
Ubuntu <ubuntu@ip-172-31-37-23.us-west-2.compute.internal>
2016-12-21 23:00:04 +0000

    added products and started order number generation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   def create
   	@cart_items = ProductCart.where(user:current_user)
+	number = (0..10).map { (rand(1...10)) }.join
 	@cart_items.each do |item|
 		ProductOrder.create(product:item.product, user:current_user, quantity:item.quantity)
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20161220044019) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
+    t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
